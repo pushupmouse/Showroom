5c6749b898efc680eedc45f76e21cb099bae1061
pushupmouse <vutranhungduc@gmail.com>
2024-09-05 17:46:08 +0700

    UI game menu options

--- Assets/Game/Script/ActivateTeleportationRay.cs
+++ Assets/Game/Script/ActivateTeleportationRay.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ActivateTeleportationRay : MonoBehaviour
 {
@@ -11,10 +12,17 @@ public class ActivateTeleportationRay : MonoBehaviour
     [SerializeField] private InputActionProperty rightActivate;
     [SerializeField] private InputActionProperty leftCancel;
     [SerializeField] private InputActionProperty rightCancel;
+    [SerializeField] private XRRayInteractor leftRayInteractor;
+    [SerializeField] private XRRayInteractor rightRayInteractor;
+
+    private bool isLeftRayHovering, isRightRayHovering;
 
     private void Update()
     {
-        leftTeleportationRay.SetActive(leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTeleportationRay.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
+        isLeftRayHovering = leftRayInteractor.TryGetHitInfo(out Vector3 leftPos, out Vector3 leftNormal, out int leftLine, out bool leftValid);
+        isRightRayHovering = rightRayInteractor.TryGetHitInfo(out Vector3 rightPos, out Vector3 rightNormal, out int rightLine, out bool rightValid);
+
+        leftTeleportationRay.SetActive(!isLeftRayHovering && leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+        rightTeleportationRay.SetActive(!isRightRayHovering && rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
