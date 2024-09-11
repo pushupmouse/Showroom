3fd0c46b9a7d0effaf945327647db67d6238fc88
pushupmouse <vutranhungduc@gmail.com>
2024-09-11 17:41:26 +0700

    add and saving to playerprefs

--- Assets/Game/Script/StudentManager.cs
+++ Assets/Game/Script/StudentManager.cs
@@ -10,36 +10,29 @@ public class StudentManager : MonoBehaviour
     [SerializeField] private Transform entryTemplate;
     [SerializeField] private GameObject grid;
 
-    private List<Student> students = new List<Student>();
 
     private void Start()
     {
-        //hard init
-        for (int i = 0; i < 20; i++)
-        {
-            float randomGrade = Random.Range(0.0f, 10.0f);
-            double roundedGrade = Math.Round(randomGrade, 1);
-            Student student = new Student("Nguyen Van A" + i, roundedGrade);
+        //getting the student list from player prefs
+        string jsonString = PlayerPrefs.GetString("Student Entries");
+        StudentEntries studentEntries = JsonUtility.FromJson<StudentEntries>(jsonString);
 
-            students.Add(student);
-        }
-
-        //sort
-        for (int  i = 0; i < students.Count; i++)
+        //sort the list
+        for (int i = 0; i < studentEntries.studentEntryList.Count; i++)
         {
-            for(int j = i + 1; j < students.Count; j++)
+            for (int j = i + 1; j < studentEntries.studentEntryList.Count; j++)
             {
-                if (students[i].Grade < students[j].Grade)
+                if (studentEntries.studentEntryList[i].Grade < studentEntries.studentEntryList[j].Grade)
                 {
-                    Student tmp = students[i];
-                    students[i] = students[j];
-                    students[j] = tmp;
+                    Student tmp = studentEntries.studentEntryList[i];
+                    studentEntries.studentEntryList[i] = studentEntries.studentEntryList[j];
+                    studentEntries.studentEntryList[j] = tmp;
                 }
             }
         }
 
-        //display
-        for(int i = 0; i < students.Count; i++)
+        //display from the list
+        for (int i = 0; i < studentEntries.studentEntryList.Count; i++)
         {
             Transform newEntry = Instantiate(entryTemplate, grid.transform);
 
@@ -56,8 +49,44 @@ public class StudentManager : MonoBehaviour
             }
 
             newEntry.Find("Rank Text").GetComponent<TextMeshProUGUI>().text = rankString;
-            newEntry.Find("Name Text").GetComponent<TextMeshProUGUI>().text = students[i].Name;
-            newEntry.Find("Grade Text").GetComponent<TextMeshProUGUI>().text = students[i].Grade.ToString();
+            newEntry.Find("Name Text").GetComponent<TextMeshProUGUI>().text = studentEntries.studentEntryList[i].Name;
+            newEntry.Find("Grade Text").GetComponent<TextMeshProUGUI>().text = studentEntries.studentEntryList[i].Grade.ToString();
+        }
+    }
+
+    private void AddStudent(string name, double grade)
+    {
+        //create
+        Student student = new Student(name, grade);
+
+        //load
+        string jsonString = PlayerPrefs.GetString("Student Entries");
+        StudentEntries studentEntries = JsonUtility.FromJson<StudentEntries>(jsonString);
+
+        //add to the list
+        studentEntries.studentEntryList.Add(student);
+
+        //save updated list
+        string json = JsonUtility.ToJson(studentEntries);
+        PlayerPrefs.SetString("Student Entries", json);
+        PlayerPrefs.Save();
+    }
+
+    private class StudentEntries
+    {
+        public List<Student> studentEntryList;
+    }
+
+    [Serializable]
+    private class Student
+    {
+        public string Name;
+        public double Grade;
+
+        public Student(string name, double grade)
+        {
+            Name = name;
+            Grade = grade;
         }
     }
 }
